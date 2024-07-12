af8cb324661cc82ff6307c48a8fc7a0fe073b7dc
Van Duong <van.duong@stickyeyes.com>
2024-07-12 21:40:30 +0100

    feat: updated sanity config

--- sanity.config.ts
+++ sanity.config.ts
@@ -7,8 +7,8 @@ export default defineConfig({
   name: 'default',
   title: 'hotel-management',
 
-  projectId: 'gyegmcbu',
-  dataset: 'production',
+  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID as string,
+  dataset: process.env.NEXT_PUBLIC_SANITY_DATASETD as string,
 
   basePath: "/studio",
 
