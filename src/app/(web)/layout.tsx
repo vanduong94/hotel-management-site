e67a4021f0a1348fd62bbc8f168278ea04e3c099
Van Duong <van.duong@stickyeyes.com>
2024-07-12 20:56:01 +0100

    feat: integrate sanity cms with next

--- src/app/(web)/layout.tsx
+++ src/app/(web)/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
 import "./globals.css";
-import Header from "../components/header";
-import Footer from "../components/footer";
+import Header from "../../components/header";
+import Footer from "../../components/footer";
 import ThemeProvider from "@/components/theme-provider/theme-provider";
 
 const poppins = Poppins({ 
