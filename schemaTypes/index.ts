858a34c0b2b166890c3e8efb8674891b342acd2c
Van Duong <van.duong@stickyeyes.com>
2024-07-13 23:10:08 +0100

    feat: import schemas

--- schemaTypes/index.ts
+++ schemaTypes/index.ts
@@ -1,1 +1,6 @@
-export const schemaTypes = []
+import account from "./account";
+import booking from "./booking";
+import hotelRoom from "./hotelRoom";
+import user from "./user";
+
+export const schemaTypes = [user, account, booking, hotelRoom]
