a27a5a357a57ab26d4f65fe167494a13c499fdd3
van duong <vanduong@vans-MacBook-Pro.local>
2024-07-17 22:04:11 +0100

    feat: created page search component

---
+++ src/components/page-search/page-search.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Search from '../search'
+
+type Props = {}
+
+function PageSearch(props: Props) {
+  return (
+    <>
+      <Search />
+    </>
+  )
+}
+
+export default PageSearch
