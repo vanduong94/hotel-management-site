aace1ccef5da0417bb723366f4630ec7e47aa77b
van duong <vanduong@vans-MacBook-Pro.local>
2024-07-17 22:04:36 +0100

    feat: import page search component to page

--- src/app/(web)/page.tsx
+++ src/app/(web)/page.tsx
@@ -1,9 +1,14 @@
 import HeroSection from "@/components/hero-section";
+import PageSearch from "@/components/page-search";
 
 const Home = () => {
   return (
     <>
       <HeroSection />
+      <PageSearch />
+      {/* Feature room */}
+      {/* Gallery */}
+      {/* Newsletter */}
     </>
   );
 }
