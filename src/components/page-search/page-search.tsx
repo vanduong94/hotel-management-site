1ee3ea5db4e95f138057523b9b39b5da697ed63f
van duong <vanduong@vans-MacBook-Pro.local>
2024-07-19 22:38:58 +0100

    feat: passed props to search component

--- src/components/page-search/page-search.tsx
+++ src/components/page-search/page-search.tsx
@@ -1,14 +1,21 @@
-import React from 'react'
+"use client"
+
+import React, { useState } from 'react'
 import Search from '../search'
 
 type Props = {}
 
 function PageSearch(props: Props) {
+  const [roomTypeFilter, setRoomTypeFilter] = useState("")
+  const [searchQuery, setSearchQuery] = useState("")
+
   return (
-    <>
-      <Search />
-    </>
-  )
+  <Search 
+    roomTypeFilter={roomTypeFilter} 
+    searchQuery={searchQuery}
+    setRoomTypeFilter={setRoomTypeFilter}
+    setSearchQuery={setSearchQuery}
+  />)
 }
 
 export default PageSearch
