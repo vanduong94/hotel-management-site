e63561db593e03069e5668fb3daae885f952270b
van duong <vanduong@vans-MacBook-Pro.local>
2024-07-26 22:47:47 +0100

    feat: updated storybook settings

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,7 +1,10 @@
 import type { StorybookConfig } from "@storybook/nextjs";
 
 const config: StorybookConfig = {
-  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+  stories: [
+    "../src/components/**/*.stories.ts", 
+    "../src/components/**/*.stories.tsx"
+  ],
   addons: [
     "@storybook/addon-onboarding",
     "@storybook/addon-links",
--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,4 +1,5 @@
 import type { Preview } from "@storybook/react";
+import "../src/app/style/global.css"
 
 const preview: Preview = {
   parameters: {
