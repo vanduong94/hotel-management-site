f0c9ebd50edd8509feffd0706ac32f19d74cd14a
van duong <vanduong@vans-MacBook-Pro.local>
2024-07-26 23:34:59 +0100

    feat: updated button stories

--- src/components/button/button.stories.tsx
+++ src/components/button/button.stories.tsx
@@ -1,12 +1,21 @@
-import type { Meta, StoryObj } from '@storybook/react';
-import { fn } from '@storybook/test';
-import Button from '@/components/button';
+import type { Meta, StoryObj } from "@storybook/react";
+import { fn } from "@storybook/test";
+import Button from "@/components/button";
 
 // More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
 const defaultArgs = {
-  title: 'Atom/Button',
+  title: "Atom/Button",
   component: Button,
-  tags: ['autodocs'],
+  tags: ["autodocs"],
+  argTypes: {
+    onClick: { action: "clicked" },
+    isPrimary: { control: "boolean" },
+    href: {
+      control: "text",
+      description: "Specifies the URL for an anchor button",
+      defaultValue: "https://example.com",
+    },
+  },
 } satisfies Meta<typeof Button>;
 
 export default defaultArgs;
@@ -17,6 +26,24 @@ export const Primary: Story = {
   args: {
     isPrimary: true,
     children: "This is a button",
-    className: "bg-primary inline-block text-center w-full py-4 rounded-xl text-white text-xl font-bold hover:-translate-y-2 hover:shadow-lg transition-all duration-500 uppercase"
+    className:
+      "bg-primary inline-block text-center w-full py-4 rounded-xl text-white text-xl font-bold hover:-translate-y-2 hover:shadow-lg transition-all duration-500 uppercase",
+  },
+};
+
+export const Tertiary: Story = {
+  args: {
+    isPrimary: false,
+    children: "This is a button",
+    className:
+      "btn-tertiary inline-block text-center w-full py-4 rounded-xl text-white text-xl font-bold hover:-translate-y-2 hover:shadow-lg transition-all duration-500 uppercase",
+  },
+};
+
+export const Link: Story = {
+  args: {
+    href: "https://example.com",
+    isPrimary: false,
+    children: "This is a link",
   },
 };
