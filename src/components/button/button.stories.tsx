aa93037b3f421ecc36aeacdab98a1e6f322426d8
van duong <vanduong@vans-MacBook-Pro.local>
2024-08-01 22:57:50 +0100

    feat: updated button stories

--- src/components/button/button.stories.tsx
+++ src/components/button/button.stories.tsx
@@ -9,7 +9,7 @@ const defaultArgs = {
   tags: ["autodocs"],
   argTypes: {
     onClick: { action: "clicked" },
-    isPrimary: { control: "boolean" },
+    buttonType: { control: "text" },
     href: {
       control: "text",
       description: "Specifies the URL for an anchor button",
@@ -24,28 +24,23 @@ type Story = StoryObj<typeof defaultArgs>;
 // More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
 export const Primary: Story = {
   args: {
-    isPrimary: true,
+    buttonType: "btn-primary",
     children: "This is a button",
-    className:
-      "bg-primary inline-block text-center w-full py-4 rounded-xl text-white text-xl font-bold hover:-translate-y-2 hover:shadow-lg transition-all duration-500 uppercase",
   },
 };
 
 export const Tertiary: Story = {
   args: {
-    isPrimary: false,
+    buttonType: "btn-tertiary",
     children: "This is a button",
-    className:
-      "btn-tertiary inline-block text-center w-full py-4 rounded-xl text-white text-xl font-bold hover:-translate-y-2 hover:shadow-lg transition-all duration-500 uppercase",
   },
 };
 
 export const Link: Story = {
   args: {
     href: "https://example.com",
-    isPrimary: false,
+    buttonType: "btn-outline",
     children: "This is a link",
-    className:
-      "bg-primary inline-block text-center w-full py-4 rounded-xl text-white text-xl font-bold hover:-translate-y-2 hover:shadow-lg transition-all duration-500 uppercase",
+    target: "_blank"
   },
 };
