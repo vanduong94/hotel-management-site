d75ef2af5b5677496731a815997dd5787a444bed
van duong <vanduong@vans-MacBook-Pro.local>
2024-08-01 21:02:18 +0100

    feat: import fontAwesome cdn

--- src/app/(web)/layout.tsx
+++ src/app/(web)/layout.tsx
@@ -27,6 +27,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" crossOrigin="anonymous"/>
+      </head>
       <body className={poppins.className}>
         <Toast />
         <main className="font-normal">
