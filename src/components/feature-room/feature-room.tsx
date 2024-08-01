0f81f548a9ba79fc2b190e063aa098b670e6cdd4
van duong <vanduong@vans-MacBook-Pro.local>
2024-08-01 22:45:14 +0100

    feat: replaces classname with buttonType

--- src/components/feature-room/feature-room.tsx
+++ src/components/feature-room/feature-room.tsx
@@ -57,7 +57,6 @@ const FeatureRoom: FC<Props> = ({ featuredRoom }) => {
           </div>
           <Button
             href={`/rooms/${featuredRoom.slug.current}`}
-            className="border h-fir text-center border-tertiary-dark text-tertiary-dark px-3 py-2 lg:py-5 lg:px-7 rounded-2xl font-bold lg:text-xl"
             buttonType="btn-outline"
           >
             More Details
