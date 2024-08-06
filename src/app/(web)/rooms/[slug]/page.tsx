16c946d4fb14660070b99d667e4741481021df6b
van duong <vanduong@vans-MacBook-Pro.local>
2024-08-06 21:39:41 +0100

    feat: added Amenities checks

--- src/app/(web)/rooms/[slug]/page.tsx
+++ src/app/(web)/rooms/[slug]/page.tsx
@@ -37,39 +37,43 @@ const RoomDetails = (props: { params: { slug: string } }) => {
               <h2 className="font-bold text-left text-lg md:text-2xl">
                 {room.name} ({room.dimension})
               </h2>
-              <div className="flex my-11">
-                {room.offeredAmenities.map((amenity) => (
-                  <div
-                    key={amenity._key}
-                    className="md:w-44 w-fit text-center px-2 md:px-0 h-20 md:h-40 mr-3 bg[#eff0f2] dark:bg-gray-800 rounded-lg grid place-content-center"
-                  >
-                    <i className={`fa-solid ${amenity.icon} md:text-2xl`}></i>
-                    <p className="text-xs md:text-base pt-3">
-                      {amenity.amenity}
-                    </p>
-                  </div>
-                ))}
-              </div>
-              <div className="mb-11">
-                <h2 className="font-bold text-3xl mb-2">Description</h2>
-                <p>{room.description}</p>
-              </div>
-              <div className="mb-11">
-                <h2 className="font-bold text-3xl mb-2">Offered Amenities</h2>
-                <div className="grid grid-cols-2">
+              {room.offeredAmenities && (
+                <div className="flex my-11">
                   {room.offeredAmenities.map((amenity) => (
                     <div
                       key={amenity._key}
-                      className="flex items-center md:my-0 my-1"
+                      className="md:w-44 w-fit text-center px-2 md:px-0 h-20 md:h-40 mr-3 bg[#eff0f2] dark:bg-gray-800 rounded-lg grid place-content-center"
                     >
-                      <i className={`fa-solid ${amenity.icon}`}></i>
-                      <p className="text-xs md:text-base ml-2">
+                      <i className={`fa-solid ${amenity.icon} md:text-2xl`}></i>
+                      <p className="text-xs md:text-base pt-3">
                         {amenity.amenity}
                       </p>
                     </div>
                   ))}
                 </div>
+              )}
+              <div className="mb-11">
+                <h2 className="font-bold text-3xl mb-2">Description</h2>
+                <p>{room.description}</p>
               </div>
+              {room.offeredAmenities && (
+                <div className="mb-11">
+                  <h2 className="font-bold text-3xl mb-2">Offered Amenities</h2>
+                  <div className="grid grid-cols-2">
+                    {room.offeredAmenities.map((amenity) => (
+                      <div
+                        key={amenity._key}
+                        className="flex items-center md:my-0 my-1"
+                      >
+                        <i className={`fa-solid ${amenity.icon}`}></i>
+                        <p className="text-xs md:text-base ml-2">
+                          {amenity.amenity}
+                        </p>
+                      </div>
+                    ))}
+                  </div>
+                </div>
+              )}
               <div className="mb-11">
                 <h2 className="font-bold text-3xl mb-2">Safety And Hygiene</h2>
                 <div className="grid grid-cols-2">
